707da84ce62fdd4f51c6cbcd4027b5160ae20cd5
Emre Sert <emresert1233@gmail.com>
2019-02-27 23:42:02 +0100

    font awesome is added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+// Routings can set here !
+  {
+  path:'',component:HomeComponent
+},
+{
+  path:'about', component:AboutComponent
+},
+{
+  path:'**', component:NotFoundComponent // ** is used when there is no url like that
+}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import {MatButtonModule, MatCheckboxModule,MatInputModule} from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatButtonModule,MatCheckboxModule,
+    AngularFontAwesomeModule,MatInputModule,MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
